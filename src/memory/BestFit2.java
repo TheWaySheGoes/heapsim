1866c717a909b1380ed765e1ea0fc5cae7dca8bb
Lukasz <lukasz.kurasinski@gmail.com>
2018-11-09 13:32:29 +0100

    some new comments added

--- src/memory/BestFit2.java
+++ src/memory/BestFit2.java
@@ -5,11 +5,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Set;
 
-
-
-
 /**
-
+ * Exactly the same as first fit with one difference in alloc
+ * 
  * @author "Lukasz Kurasinski"
  * @since 1.0
  */
@@ -17,24 +15,29 @@ public class BestFit2 extends Memory {
 	private class FreeBlock {
 		int pointer;
 		int size;
+
 		public FreeBlock(int pointer, int size) {
-			this.pointer=pointer;
-			this.size=size;
+			this.pointer = pointer;
+			this.size = size;
 		}
+
 		public int getSize() {
 			return size;
 		}
+
 		public int getPointer() {
 			return pointer;
 		}
+
 		public void setPointer(int pointer) {
 			this.pointer = pointer;
 		}
+
 		public void setSize(int size) {
 			this.size = size;
 		}
 	}
-	
+
 	private ArrayList<Pointer> pointerTable = new ArrayList<>();
 	private HashMap<Pointer, Integer> pointerSizeList = new HashMap<>();
 	private ArrayList<FreeBlock> freeListTable = new ArrayList<>();
@@ -47,7 +50,7 @@ public class BestFit2 extends Memory {
 	 */
 	public BestFit2(int size) {
 		super(size);
-		freeListTable.add(new FreeBlock(0,size));
+		freeListTable.add(new FreeBlock(0, size));
 	}
 
 	/**
@@ -57,48 +60,44 @@ public class BestFit2 extends Memory {
 	 */
 	@Override
 	public Pointer alloc(int size) {
-		
-		int index=-1;
-		
+
+		int index = -1;
+
 		for (int i = 0; i < freeListTable.size(); i++) {
-			if(freeListTable.get(i).getSize()==0) {
+			if (freeListTable.get(i).getSize() == 0) {
 				freeListTable.remove(i);
 			}
 		}
 		sortAllSize();
-		//search for best fitted free block
+		// search for best fitted free block
 		for (int i = 0; i < freeListTable.size(); i++) {
-			if(freeListTable.get(i).getSize()<size) {
+			if (freeListTable.get(i).getSize() < size) {
 				continue;
-			}else{
-				index=i;
+			} else {
+				index = i;
 				break;
 			}
 		}
-			
-			
-			if(index!=-1){	
-				Pointer p = new Pointer(freeListTable.get(index).getPointer(), this);
-				freeListTable.get(index).setSize(freeListTable.get(index).getSize() - size);
-				freeListTable.get(index).setPointer(freeListTable.get(index).getPointer() + size);
-					
-				pointerTable.add(p);
-				pointerSizeList.put(p, size);
-				
-				return p;
-			}
-		
-		
-		
+
+		if (index != -1) {
+			Pointer p = new Pointer(freeListTable.get(index).getPointer(), this);
+			freeListTable.get(index).setSize(freeListTable.get(index).getSize() - size);
+			freeListTable.get(index).setPointer(freeListTable.get(index).getPointer() + size);
+
+			pointerTable.add(p);
+			pointerSizeList.put(p, size);
+
+			return p;
+		}
+
 		return null;
 	}
 
 	private void sortAllSize() {
-		Collections.sort(freeListTable,
-				(a, b) -> a.getSize() < b.getSize() ? -1 : a.getSize() == b.getSize() ? 0 : 1);
+		Collections.sort(freeListTable, (a, b) -> a.getSize() < b.getSize() ? -1 : a.getSize() == b.getSize() ? 0 : 1);
 
 	}
-	
+
 	private void sortAll() {
 		Collections.sort(pointerTable,
 				(a, b) -> a.pointsAt() < b.pointsAt() ? -1 : a.pointsAt() == b.pointsAt() ? 0 : 1);
@@ -106,6 +105,7 @@ public class BestFit2 extends Memory {
 				(a, b) -> a.getPointer() < b.getPointer() ? -1 : a.getPointer() == b.getPointer() ? 0 : 1);
 
 	}
+
 	/**
 	 * Releases a number of data cells, delete data erase pointer from lists
 	 * 
@@ -114,33 +114,31 @@ public class BestFit2 extends Memory {
 	 */
 	@Override
 	public void release(Pointer p) {
-		
 
 		freeListTable.add(new FreeBlock(p.pointsAt(), pointerSizeList.get(p)));
-	
-	
-	pointerTable.remove(p);
-	pointerSizeList.remove(p);
-	merge();
-}
 
+		pointerTable.remove(p);
+		pointerSizeList.remove(p);
+		merge();
+	}
 
-private void merge() {
-	sortAll();
-	
-	for (int i = 0; i <freeListTable.size()-1; i++) {
-		if(freeListTable.get(i).getPointer()+freeListTable.get(i).getSize()==freeListTable.get(i+1).getPointer()) {
-			freeListTable.add(new FreeBlock(freeListTable.get(i).getPointer(),freeListTable.get(i).getSize()+freeListTable.get(i+1).getSize()));
-			freeListTable.remove(i);
-			freeListTable.remove(i);
-			sortAll();
-			
-			i=0;
-			continue;
+	private void merge() {
+		sortAll();
+
+		for (int i = 0; i < freeListTable.size() - 1; i++) {
+			if (freeListTable.get(i).getPointer() + freeListTable.get(i).getSize() == freeListTable.get(i + 1)
+					.getPointer()) {
+				freeListTable.add(new FreeBlock(freeListTable.get(i).getPointer(),
+						freeListTable.get(i).getSize() + freeListTable.get(i + 1).getSize()));
+				freeListTable.remove(i);
+				freeListTable.remove(i);
+				sortAll();
+
+				i = 0;
+				continue;
+			}
 		}
 	}
-}
-
 
 	@Override
 	public void compact() {
@@ -156,18 +154,22 @@ private void merge() {
 	@Override
 	public void printLayout() {
 		for (int i = 0; i < freeListTable.size(); i++) {
-			if(freeListTable.get(i).getSize()==0) {
+			if (freeListTable.get(i).getSize() == 0) {
 				freeListTable.remove(i);
 			}
 		}
 		sortAll();
 		System.out.println(">>>>>>>>>>>empty<<<<<<<<<<<");
 		for (int i = 0; i < freeListTable.size(); i++) {
-			System.out.println((i)+". points at "+freeListTable.get(i).getPointer()+" to: "+(freeListTable.get(i).getPointer()+freeListTable.get(i).getSize()-1)+" size "+freeListTable.get(i).getSize());
+			System.out.println((i) + ". points at " + freeListTable.get(i).getPointer() + " to: "
+					+ (freeListTable.get(i).getPointer() + freeListTable.get(i).getSize() - 1) + " size "
+					+ freeListTable.get(i).getSize());
 		}
 		System.out.println("<<<<<<<<<<<allocated>>>>>>>>>>>>");
 		for (int i = 0; i < pointerTable.size(); i++) {
-			System.out.println((i)+". points at "+pointerTable.get(i).pointsAt()+" to: "+(pointerTable.get(i).pointsAt()+pointerSizeList.get(pointerTable.get(i))-1)+ " size "+pointerSizeList.get(pointerTable.get(i)));
+			System.out.println((i) + ". points at " + pointerTable.get(i).pointsAt() + " to: "
+					+ (pointerTable.get(i).pointsAt() + pointerSizeList.get(pointerTable.get(i)) - 1) + " size "
+					+ pointerSizeList.get(pointerTable.get(i)));
 		}
 		System.out.println("---------------------");
 	}
--- src/memory/FirstFit2.java
+++ src/memory/FirstFit2.java
@@ -13,6 +13,7 @@ import java.util.Set;
  */
 public class FirstFit2 extends Memory {
 
+	// object describing a free memory block
 	private class FreeBlock {
 		int pointer;
 		int size;
@@ -39,13 +40,15 @@ public class FirstFit2 extends Memory {
 		}
 	}
 
+	// those two for printing purposes mainly
 	private ArrayList<Pointer> pointerTable = new ArrayList<>();
 	private HashMap<Pointer, Integer> pointerSizeList = new HashMap<>();
 
 	private LinkedList<FreeBlock> freeListTable = new LinkedList<>();
 
 	/**
-	 * Initializes an instance of a first fit-based memory.
+	 * Initializes an instance of a first fit-based memory. adds whole memory as a
+	 * free mem block to the table
 	 * 
 	 * @param size
 	 *            The number of cells.
@@ -63,15 +66,18 @@ public class FirstFit2 extends Memory {
 	@Override
 	public Pointer alloc(int size) {
 
+		// finds a free block big enough for the alloc
 		for (int i = 0; i < freeListTable.size(); i++) {
 			if (freeListTable.get(i).getSize() >= size) {
 				Pointer p = new Pointer(freeListTable.get(i).getPointer(), this);
 				freeListTable.get(i).setSize(freeListTable.get(i).getSize() - size);
 				freeListTable.get(i).setPointer(freeListTable.get(i).getPointer() + size);
-				
+
+				// those two for printing mainly
 				pointerTable.add(p);
 				pointerSizeList.put(p, size);
 
+				// no zero size free blocks in the freeBlockTable
 				if (freeListTable.get(i).getSize() == 0) {
 					freeListTable.remove(i);
 				}
@@ -100,38 +106,35 @@ public class FirstFit2 extends Memory {
 	 */
 	@Override
 	public void release(Pointer p) {
-		
 
-			freeListTable.add(new FreeBlock(p.pointsAt(), pointerSizeList.get(p)));
-		
-		
+		freeListTable.add(new FreeBlock(p.pointsAt(), pointerSizeList.get(p)));
 		pointerTable.remove(p);
 		pointerSizeList.remove(p);
 		merge();
 	}
 
-	
+	// merges free blocks if they are next to each other
 	private void merge() {
 		sortAll();
-		
-		for (int i = 0; i <freeListTable.size()-1; i++) {
-			if(freeListTable.get(i).getPointer()+freeListTable.get(i).getSize()==freeListTable.get(i+1).getPointer()) {
-				freeListTable.add(new FreeBlock(freeListTable.get(i).getPointer(),freeListTable.get(i).getSize()+freeListTable.get(i+1).getSize()));
+
+		for (int i = 0; i < freeListTable.size() - 1; i++) {
+			if (freeListTable.get(i).getPointer() + freeListTable.get(i).getSize() == freeListTable.get(i + 1)
+					.getPointer()) {
+				freeListTable.add(new FreeBlock(freeListTable.get(i).getPointer(),
+						freeListTable.get(i).getSize() + freeListTable.get(i + 1).getSize()));
 				freeListTable.remove(i);
 				freeListTable.remove(i);
 				sortAll();
-				
-				i=0;
+
+				i = 0;
 				continue;
 			}
 		}
 	}
 
-
 	public void compact() {
 
 	}
-
 
 	@Override
 	public void printLayout() {
@@ -139,13 +142,13 @@ public class FirstFit2 extends Memory {
 		System.out.println(">>>>>>>>>>>empty<<<<<<<<<<<");
 		for (int i = 0; i < freeListTable.size(); i++) {
 			System.out.println((i) + ". points at " + freeListTable.get(i).getPointer() + " to: "
-					+ (freeListTable.get(i).getPointer() + freeListTable.get(i).getSize()-1) + " size "
+					+ (freeListTable.get(i).getPointer() + freeListTable.get(i).getSize() - 1) + " size "
 					+ freeListTable.get(i).getSize());
 		}
 		System.out.println("<<<<<<<<<<<allocated>>>>>>>>>>>>");
 		for (int i = 0; i < pointerTable.size(); i++) {
 			System.out.println((i) + ". points at " + pointerTable.get(i).pointsAt() + " to: "
-					+ (pointerTable.get(i).pointsAt() + pointerSizeList.get(pointerTable.get(i))-1) + " size "
+					+ (pointerTable.get(i).pointsAt() + pointerSizeList.get(pointerTable.get(i)) - 1) + " size "
 					+ pointerSizeList.get(pointerTable.get(i)));
 		}
 		System.out.println("---------------------");
